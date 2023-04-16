5bc810e8c7912f8feffefc3dc0f05bd73b1d819c
Aysedeniz <tdilsen26@gmail.com>
2023-04-16 12:45:44 +0300

    main

---
+++ AnonX/utils/inline/start.py
@@ -0,0 +1,51 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+import config
+
+
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="ʙᴇɴɪ ɢʀᴜʙᴀ ᴇᴋʟᴇ",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="ᴋᴏᴍᴜᴛʟᴀʀ",
+                callback_data="settings_back_helper",
+            ),
+            InlineKeyboardButton(
+                text="ᴋᴏᴍᴜᴛʟᴀʀ", callback_data="settings_helper"
+            ),
+        ],
+     ]
+    return buttons
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="ʙᴇɴɪ ɢʀᴜʙᴀ ᴇᴋʟᴇ",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="ᴋᴏᴍᴜᴛʟᴀʀ", callback_data="settings_back_helper"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="❣ ᴛᴀᴋɪᴘᴄɪ ᴠᴇ ʀᴇᴋʟᴀᴍ ᴋᴀɴᴀʟɪ ❣", url=config.SUPPORT_GROUP
+            ),
+            InlineKeyboardButton(
+                text="🥀 ʙᴏᴛ ꜱᴀʜɪʙɪ 🥀", user_id=OWNER
+            )
+        ],
+     ]
+    return buttons
---
+++ strings/helpers.py
@@ -0,0 +1,149 @@
+HELP_1 = """🙄 **<u>ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs:</u>**
+
+/pause /durdur : Yayın Durdurur.
+
+/resume /devam : Yayın Devam Eder
+
+/skip /atla : Yayın bir sonraki parçaya atlar.
+
+/stop /son : Yayın kapatılır.
+
+/player : şarkı detayını gösterir
+
+/queue /sıra : sʜᴏᴡs ᴛʜᴇ ǫᴜᴇᴜᴇᴅ ᴛʀᴀᴄᴋs ʟɪsᴛ."""
+
+HELP_2 = """😜 **<u>auth kullanıcısı :</u>**
+auth kullanıcısı, sohbette yönetici hakları olmadan botta yönetici haklarını kullanabilir. [sadece yöneticiler]
+
+/auth [ᴜsᴇʀɴᴀᴍᴇ] : botun yetkili listesine bir kullanıcı ekleyin.
+
+/unauth [ᴜsᴇʀɴᴀᴍᴇ] : bir yetki kullanıcısını yetki istesinden çıkartın.
+
+/authusers : grubun yetkili kullanıcıların listesini gösterir."""
+
+HELP_3 = """<b>kara liste özelliği</b> [ᴏɴʟʏ ғᴏʀ sᴜᴅᴏᴇʀs]
+😒 **<u>ʙʟᴀᴄᴋʟɪsᴛ ᴄʜᴀᴛ :</u>**
+
+/blacklistchat [ᴄʜᴀᴛ ɪᴅ] : Bir sohbetin botu kullanmasını kara listeye alın.
+
+/whitelistchat [ᴄʜᴀᴛ ɪᴅ] : kara listeye alınan sohbeti beyaz listeye alın.
+
+/blacklistedchat : kara listeye alınmış sohbetlerin listesini gösterir.
+
+
+😤 **<u>Kullanıcıları Engelle:</u>**
+
+/block [ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ʀᴇᴩʟʏ ᴛᴏ ᴀ ᴄʜᴜᴛɪʏᴀ] : botu görmezden gelmeye başlar, böylece bot komutlarını kullanamaz
+
+/unblock [ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ʀᴇᴩʟʏ ᴛᴏ ᴀ ᴜsᴇʀ] : Engellenen kullanıcının engelini kaldırır.
+
+/blockedusers : karalisteye alınan kullanıcıları gösterir."""
+
+HELP_4 = """🍒 **<u>Reklam komutları</u>** [ᴏɴʟʏ ғᴏʀ sᴜᴅᴏᴇʀs] :
+
+/broadcast : Broadcast komutudur komutudur.
+
+<u>Broadcast extra komutları:</u>
+**-pin** 
+**-pinloud** 
+**-user**  
+**-assistant** 
+**-nobot** 
+
+**Örnek:** `/broadcast -user -assistant -pin  TEST ...`
+"""
+HELP_5 = """😉 **<u>Extralar :</u>**
+
+/loop [Disable/Enable] veya [between 1:10] 
+
+/shuffle /karıştır
+
+/seek /Ara 
+
+/seekback : Geri Dön komutudur.
+
+/lyrics [sᴏɴɢ ɴᴀᴍᴇ] : şarkı sözleri."""
+
+HELP_6 = """🤨 **<u>Playlist Komutları :</u>**
+
+/playlist 
+
+/deleteplaylist 
+
+/play /oynat """
+
+HELP_7 = """🍑 **Ping Komutları :**
+
+/ping 
+
+/stats """
+
+HELP_8 = """💞 **<u>Oynatma Komutları:</u>**
+
+**c** Videosuz Oynatır
+**v** Videolu Oynatır
+
+/oynat veya /voynat (biri videoludur , biri videosuz oynatır.)
+/channelplay """
+
+HELP_9 = """🥀 **<u>Sudo Komutları :</u>**
+
+🥺 **<u>Ekle & Çıkart komutları :</u>**
+
+/addsudo 
+/delsudo 
+
+🥶 **<u>Heroku :</u>**
+
+/usage 
+
+🤯 **<u>yapılandırma değişkenleri:</u>**
+
+/get_var : 
+/del_var : 
+/set_var [var name] [value] : 
+
+🤓 **<u>ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs:</u>**
+
+/restart 
+
+/update 
+
+/speedtest 
+
+/maintenance [ᴇɴᴀʙʟᴇ/ᴅɪsᴀʙʟᴇ] 
+
+/logger [ᴇɴᴀʙʟᴇ/ᴅɪsᴀʙʟᴇ] 
+
+/get_log [ɴᴜᴍʙᴇʀ ᴏғ ʟɪɴᴇs] 
+
+💔 **<u>ғᴏʀ ᴩʀɪᴠᴀᴛᴇ ʙᴏᴛ ᴏɴʟʏ :</u>**
+
+/authorize [ᴄʜᴀᴛ ɪᴅ] 
+/unauthorize [ᴄʜᴀᴛ ɪᴅ] 
+/authorized """
+
+HELP_10 = """🤑 **<u>ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛs :</u>**
+
+/activevoice 
+/activevideo 
+/autoend [ᴇɴᴀʙʟᴇ|ᴅɪsᴀʙʟᴇ] """
+
+HELP_11 = """😅**<u>ɢᴇᴛ sᴛᴀʀᴛᴇᴅ ᴡɪᴛʜ ʙᴏᴛ</u>**
+/start 
+
+/help 
+
+/reboot 
+
+/settings 
+
+/sudolist """
+
+HELP_12 = """🤬 **<u>ɢʙᴀɴ ғᴇᴀᴛᴜʀᴇ</u>** [ᴏɴʟʏ ғᴏʀ sᴜᴅᴏᴇʀs] :
+
+/gban [ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ʀᴇᴩʟʏ ᴛᴏ ᴀ ᴄʜᴜᴛɪʏᴀ] : 
+
+/ungban [ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ʀᴇᴩʟʏ ᴛᴏ ᴀ ᴜsᴇʀ] :
+
+/gbannedusers """
