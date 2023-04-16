59dfbe0043748f8749d1b803f4b7028f935c0597
Aysedeniz <94296804+deryadenizzz@users.noreply.github.com>
2023-04-16 16:18:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "23825467"))
-API_HASH = getenv("API_HASH", "5e699f0de1f4ef172ef98cf3d4811395")
+API_ID = int(getenv("API_ID", "27910486"))
+API_HASH = getenv("API_HASH", "c8ef26df3c3afc516d07886271984881")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "6098364034:AAFOXRzMgLvRdPezGoSryU1zcPu4sHM78P0")
+BOT_TOKEN = getenv("BOT_TOKEN", "6098364034:AAF3ssrpxB8m5LACpoXoAp7WU9oKua0JiME")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://tansel266:tansel1430@cluster0.nbhxbsl.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://tansel26:tansel1430@cluster0.lrplsud.mongodb.net/?retryWrites=true&w=majority")
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001949045877"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "DamlaMusicBot")
 
